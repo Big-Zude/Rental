e7926c445fe149887d4ceda8e0d1574dbf2f067c
Zude Mwango <zudemwango@gmail.com>
2021-12-22 19:14:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { AmplifyProvider } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+import { RentalCollection } from './ui-components'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AmplifyProvider>
+     <RentalCollection
+ type='list' overrides={{
+   'Collection.CardA[0]': {
+       overrides: {
+        'Flex.Image[0]': { objectFit: 'cover' }
+       }
+    }
+ }}
+/>
+    </AmplifyProvider>
+  )
 }
 
-export default App;
+export default App
